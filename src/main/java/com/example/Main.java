6c52fe211aa5cb1721e0c8e73c6567fcc3278bd6
Юлия Алексина <uliyaalexina@mail.ru>
2023-04-27 20:57:00 -0700

    Added src/

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Вячеслав";
+        if (name == "Вячеслав") {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+
+    }
+
+}
